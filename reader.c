2b42a24503e2f141ef23dae7f6e5093c95f939ca
aparnasusan <43822908+aparnasusan@users.noreply.github.com>
2020-03-03 00:05:12 +0530

    Add files via upload

---
+++ reader.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include<stdlib.h> 
+#include<time.h> 
+#include<unistd.h>
+
+
+sem_t wrt;
+sem_t mutex;
+int rc = 0;
+int val=1;
+
+void *reader(void* k)
+{
+
+	for(int j=0;j<5;j++)
+	{
+		sleep(rand()%5);
+		sem_wait(&mutex);
+		rc ++;
+		if (rc == 1)
+			sem_wait(&wrt);
+		sem_post(&mutex);
+		printf("Reader %d reads %d\n",*((int *)k),val);
+		sem_wait(&mutex);
+		rc --;
+		if (rc == 0)
+			sem_post(&wrt);
+		sem_post(&mutex);
+	}
+
+}
+
+
+void *writer(void* k)
+{
+	for(int j=0;j<5;j++)
+	{
+		sleep(rand()%5);
+		
+		sem_wait(&wrt);
+		val=rand()%10+1;
+		printf("Writer %d writes %d\n",*((int *)k),val);
+		sem_post(&wrt);
+		
+	}
+
+}
+
+
+
+void main()
+{
+	int i,k;
+	pthread_t read[5],write[5];
+	sem_init(&mutex, 0,1);
+    	sem_init(&wrt,0,1);
+	srand(time(0));
+    	int a[5];
+    	for(i = 0; i <5; i++) 
+	{
+		a[i]=i;
+        	pthread_create(&read[i], NULL,reader,(void *)&a[i]);
+        	pthread_create(&write[i], NULL,writer,(void *)&a[i]);
+    	}
+    
+    	for(i = 0; i < 5; i++) 
+	{
+        	pthread_join(read[i], NULL);
+    
+        	pthread_join(write[i], NULL);
+    	}
+
+    	sem_destroy(&mutex);
+    	sem_destroy(&wrt);
+}
